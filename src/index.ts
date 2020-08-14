819b643973a325084814848b3d293faa808c5ebe
derekjqwang <derekjqwang@tencent.com>
2020-08-14 14:17:38 +0800

    feat: 暴露类型

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { DiffParser, LcovParser, LogParser } from './parsers';
 export { getIncrease, getIncreaseSync, FormatData } from './apis';
+export { Locv, DetailLines, Total } from './types';
