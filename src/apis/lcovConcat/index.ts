345a9866b54dff1df5ad03203161b2325bf0e12c
derekjqwang <derekjqwang@tencent.com>
2020-08-17 16:39:30 +0800

    feat: 增加初次提交信息获取

--- src/apis/lcovConcat/index.ts
+++ src/apis/lcovConcat/index.ts
@@ -1,8 +1,21 @@
+/* eslint-disable no-restricted-syntax */
 import { Lcov } from '../../types';
 import { LcovConcat } from './concat';
+import { LcovParser } from '../../parsers';
 
 export * from './concat';
 
-export function lcovConcat(...lcov: Lcov[]): Lcov {
-  return new LcovConcat().concat(...lcov).getRes();
+/**
+ * 输出合并之后的文件
+ * @param path lcov文件路径
+ */
+export async function lcovConcat(...path: string[]): Promise<Lcov> {
+  const parserPromise: Promise<Lcov>[] = [];
+  for (const item of path) {
+    parserPromise.push(new LcovParser(item).run());
+  }
+
+  const res = await Promise.all(parserPromise);
+
+  return new LcovConcat().concat(...res).getRes();
 }
