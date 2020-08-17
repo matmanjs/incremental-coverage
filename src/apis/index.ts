8db88f345be500a12dc5a1d1da07abeb7d1862f1
derekjqwang <derekjqwang@tencent.com>
2020-08-17 15:28:54 +0800

    feat: 增加一个函数式方法

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,2 +1,2 @@
 export { getIncrease, getIncreaseSync, FormatData } from './getIncrease';
-export { LcovConcat } from './lcovConcat';
+export { LcovConcat, lcovConcat } from './lcovConcat';
--- src/apis/lcovConcat/index.ts
+++ src/apis/lcovConcat/index.ts
@@ -1,1 +1,8 @@
+import { Lcov } from '../../types';
+import { LcovConcat } from './concat';
+
 export * from './concat';
+
+export function lcovConcat(...lcov: Lcov[]): Lcov {
+  return new LcovConcat().concat(...lcov).getRes();
+}
