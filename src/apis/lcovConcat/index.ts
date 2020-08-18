9c0fc8190a429f7d84c167b320ea5e4101da49e6
derekjqwang <derekjqwang@tencent.com>
2020-08-18 10:00:13 +0800

    fix: 文件重复合并问题

--- src/apis/lcovConcat/index.ts
+++ src/apis/lcovConcat/index.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-restricted-syntax */
+import path from 'path';
 import { Lcov } from '../../types';
 import { LcovConcat } from './concat';
 import { LcovParser } from '../../parsers';
@@ -9,9 +10,16 @@ export * from './concat';
  * 输出合并之后的文件
  * @param path lcov文件路径
  */
-export async function lcovConcat(...path: string[]): Promise<Lcov> {
+export async function lcovConcat(...lcovPath: string[]): Promise<Lcov> {
+  // 去掉重复文件
+  const lcovSet = new Set<string>();
+  for (const item of lcovPath) {
+    lcovSet.add(path.resolve(item));
+  }
+
+  // 并行格式化
   const parserPromise: Promise<Lcov>[] = [];
-  for (const item of path) {
+  for (const item of lcovSet) {
     parserPromise.push(new LcovParser(item).run());
   }
 
