885f9fc2f571e7cd1218258361f22f25df136962
linjianghe <linjianghe@tencent.com>
2020-08-18 17:21:15 +0800

    feat: 新增自动化测试

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,2 +1,2 @@
 export { getIncrease, getIncreaseSync, FormatData } from './getIncrease';
-export { LcovConcat, lcovConcat } from './lcovConcat';
+export { LcovConcat, lcovConcat, increaseLcovConcat } from './lcovConcat';
---
+++ test/scripts/index.test.ts
@@ -0,0 +1,19 @@
+import 'mocha';
+import { expect } from 'chai';
+
+import * as incrementalCoverage from '../../src/index';
+
+describe('./index.ts', () => {
+  it('export should be correct', () => {
+    expect(incrementalCoverage).to.have.all.keys(
+      "DiffParser",
+      "LcovConcat",
+      "LcovParser",
+      "LogParser",
+      "getIncrease",
+      "getIncreaseSync",
+      "increaseLcovConcat",
+      "lcovConcat",
+    );
+  });
+});
