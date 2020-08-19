c52393a1948f527783931ebbf4419188dacb7763
derekjqwang <derekjqwang@tencent.com>
2020-08-19 11:11:25 +0800

    fix: 编译错误

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,2 +1,2 @@
-export { getIncrease, getIncreaseSync, FormatData } from './getIncrease';
+export { getIncrease } from './getIncrease';
 export { LcovConcat, lcovConcat, increaseLcovConcat } from './lcovConcat';
